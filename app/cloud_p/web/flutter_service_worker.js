ffd3881892d52520c834a907d6047ae57a731863
sjpark-onu <63767577+sjpark-onu@users.noreply.github.com>
2024-05-29 13:58:34 +0900

    modify

--- app/cloud_p/web/flutter_service_worker.js
+++ app/cloud_p/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "42780872018e35022ad80076e2875aa3",
+const RESOURCES = {"flutter_bootstrap.js": "d939e15d881991cb25636d488789eabb",
 "version.json": "3b92c2a65ca3331fe110b1c91565149d",
 "index.html": "43102cbfdfbf92a186c78faf564dd08d",
 "/": "43102cbfdfbf92a186c78faf564dd08d",
-"main.dart.js": "60d7631d68ca5b6cf55aa1e2f35825d1",
+"main.dart.js": "e2fdc33c86f7b768fb5bc7c1e187de8f",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "95d4fc600c279d9f4c36fa71b42f4428",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
